873743e8dd5831471fe491cadf6d4dc90b0dd1b8
Jorge Massi <j.massi2022@gmail.com>
2024-09-07 17:04:21 +0100

    commmite

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { ListingComponent } from './components/property/listing/listing.component';
+import { PropertyService } from './services/property-service/property.service';
+import { HttpClientModule } from '@angular/common/http';
+import { CardsComponent } from './components/property/cards/cards.component';
+
+
+export const routes: Routes = [
+  {path: 'listing', component:ListingComponent}, 
+  {path: 'cards', component: CardsComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes), HttpClientModule],
+  exports: [RouterModule],
+  providers: [PropertyService]
+})
+export class AppRoutingModule { }
