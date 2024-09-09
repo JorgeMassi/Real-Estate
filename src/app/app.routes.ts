5d20997bede3fcf428f9e0f3e59f2a9eda9cf516
Jorge Massi <j.massi2022@gmail.com>
2024-09-09 23:08:59 +0100

    front end

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
 import { ListingComponent } from './components/property/listing/listing.component';
 import { PropertyService } from './services/property-service/property.service';
 import { HttpClientModule } from '@angular/common/http';
 import { CardsComponent } from './components/property/cards/cards.component';
+import { FormsModule } from '@angular/forms';
 
 
 export const routes: Routes = [
@@ -13,7 +13,7 @@ export const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes), HttpClientModule],
+  imports: [RouterModule.forRoot(routes), HttpClientModule, FormsModule],
   exports: [RouterModule],
   providers: [PropertyService]
 })
